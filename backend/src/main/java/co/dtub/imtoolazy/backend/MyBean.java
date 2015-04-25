f1fdd6ade49c557de816df6bb372ebbd1db968c3
galenos <chandler@dtub.co>
2015-04-24 18:41:05 -0700

    First commit

---
+++ backend/src/main/java/co/dtub/imtoolazy/backend/MyBean.java
@@ -0,0 +1,17 @@
+package co.dtub.imtoolazy.backend;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
---
+++ backend/src/main/java/co/dtub/imtoolazy/backend/PMF.java
@@ -0,0 +1,15 @@
+package co.dtub.imtoolazy.backend;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+public final class PMF {
+    private static final PersistenceManagerFactory pmfInstance =
+            JDOHelper.getPersistenceManagerFactory("transactions-optionsl");
+
+    private PMF() {}
+
+    public static PersistenceManagerFactory get() {
+        return pmfInstance;
+    }
+}
